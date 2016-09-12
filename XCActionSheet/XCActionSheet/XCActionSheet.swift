8d7d32306d401b9a5100ebc45c6a383f7308e880
杨晓晨 <xiaochenyung@gmail.com>
2016-09-12 10:41:33 +0800

    添加分割线

--- XCActionSheet/XCActionSheet/XCActionSheet.swift
+++ XCActionSheet/XCActionSheet/XCActionSheet.swift
@@ -12,36 +12,38 @@ let cellHeight: CGFloat = 44
 
 class XCActionSheet: UIView {
     
-//    let tableView = UITableView()
-    
     let items = NSMutableArray()
     
-    
-    
-
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//    }
-//    
-//    required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-    
     func initView(title title: String,buttonTitles titles: [String],cancleBtuttonTitle: String) -> () {
-//        items.addObject(title)
         items.addObjectsFromArray(titles)
+        config()
+    }
+    
+    private func config() {
         backgroundColor = UIColor.init(red: 230, green: 230, blue: 230, alpha: 0.84)
+        addbuttons()
+        addlines()
+    }
+    
+    private func addbuttons() {
         var index: CGFloat = 0
-        for item in titles {
+        for item in items {
             let button = UIButton()
             button.frame = CGRectMake(0, cellHeight * index, UIScreen.mainScreen().bounds.size.width, cellHeight)
-            button.setTitle(item, forState: .Normal)
+            button.setTitle(item as? String, forState: .Normal)
             button.setTitleColor(UIColor.blackColor(), forState: .Normal)
-//            button.titleLabel?.font = UIFont.systemFontOfSize(15)
             addSubview(button)
             index += 1
         }
-        
+    }
+    
+    private func addlines() {
+        for index in 1..<items.count {
+            let line = UIView()
+            line.backgroundColor = UIColor.lightGrayColor();
+            addSubview(line)
+            line.frame = CGRectMake(0, 44 * CGFloat(index), UIScreen.mainScreen().bounds.size.width, 0.5)
+        }
     }
     
     func showInView(view: UIView) -> () {
